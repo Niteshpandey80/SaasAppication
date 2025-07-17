de92a742fb4cef18ba3c860de1c0675ca5fca143
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-07-17 21:59:48 +0530

    sidebar complete

--- font-end/src/components/Sidebar.jsx
+++ font-end/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useClerk, useUser } from '@clerk/clerk-react';
+import { Protect, useClerk, useUser } from '@clerk/clerk-react';
 import {
   FileText,
   FileTextIcon,
@@ -8,6 +8,7 @@ import {
   SquarePen,
   User,
   Eraser,
+  LogOutIcon,
 } from 'lucide-react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -25,7 +26,7 @@ const navItems = [
 
 const Sidebar = ({ sidebar, setSidebar }) => {
   const { user } = useUser();
-  const { signOut, OpenUserProfile } = useClerk();
+  const { signOut, openUserProfile } = useClerk();
 
   return (
     <div
@@ -35,12 +36,12 @@ const Sidebar = ({ sidebar, setSidebar }) => {
     >
       <div className='my-7 w-full'>
         <img
-          src={user.imageUrl}
+          src={user.imageUrl}  
           alt='User avatar'
           className='w-13 rounded-full mx-auto'
         />
         <h1 className='mt-1 text-center'>{user.fullName}</h1>
-        <div className='mt-4 flex flex-col gap-1'>
+        <div className='px-6 mt-5 text-sm text-gray-600 font-medium'>
           {navItems.map(({ to, label, Icon }) => (
             <NavLink
               key={to}
@@ -60,6 +61,21 @@ const Sidebar = ({ sidebar, setSidebar }) => {
             </NavLink>
           ))}
         </div>
+      </div>
+      <div className='w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between'>
+        <div onClick={openUserProfile} className='flex gap-2 items-center cursor-pointer'>
+           <img src={user.imageUrl} className='w-8 rounded-full' alt="" />
+           <div>
+            <h1 className='text-sm font-medium'>{user.fullName}</h1>
+            <p className='text-sm text-gray-500'>
+              <Protect plan='premium' fallback='Free'>
+                Premium
+              </Protect>
+              Plan
+            </p>
+           </div>
+        </div>
+         <LogOutIcon onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700 transition cursor-pointer'/>
       </div>
     </div>
   );
